fe16038e3952f52d43857911118a77706bf011e1
victoriaehrbar <victoriaehrbar@gmail.com>
2021-01-24 15:41:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
-INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
-INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
-INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
-INSERT INTO bears VALUES (5,"Melissa", 13, "F", "black", "strong", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "dark brown", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 5, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 17, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 14, "F", "black", "strong", 1);
 INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
 INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
 INSERT INTO bears VALUES (8,null, 20, "F", "light black", "fuzzy", 0);
